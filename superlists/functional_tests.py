2d2f9fe9d3b7863a27fb3d40280383f2563d7762
vitoria <vitoriarntrindade@gmail.com>
2024-05-21 09:23:31 -0400

    o teste funcional agora checa que podemos acrescentar itens 'to-do' na lista

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 import unittest
+import time
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -16,5 +19,28 @@ class NewVisitorTest(unittest.TestCase):
         #Percebe que o título menciona "To-Do"
         self.browser.get('http://localhost:8000')
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element(By.TAG_NAME,'h1').text
+        self.assertIn('To-Do', header_text)
 
+        #Maria é convidada a inserir um item de uma tarefa
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        inputbox.send_keys('Buy a new notebook')
+
+        #Quando Maria clica enter, a página é atualizada, e agora pagina lista "1: buy a notebook"
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(3)
+
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_element(By.TAG_NAME, 'tr')
+        self.assertTrue(any(row.text == '1: Buy a new notebook' for row in rows))
+
+        #Continua sendo exibida um caixa de txt a convidando para inserir outro item "to_do"
+        #Ela insere "Tidy up the house"
+        self.fail("finish the test!")
+
+        #A página é atualizada e agora existem dois itens na lista [...]
