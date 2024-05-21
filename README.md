cee6a0d8383b9381a319febbd067ef0e06ad261d
vitoria <vitoriarntrindade@gmail.com>
2024-05-21 06:32:58 -0400

    Adicionado primeiro teste funcional usando unit test e adicionado .gitignore

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# TDD_Python_Django
+# TDD (Test Driven Development) with Python
+
+## Este projeto tem como objetivo documentar os meus estudos usando o livro **TDD with Python** do autor Harry J. W. Percival 
+
+
+![TDD](https://m.media-amazon.com/images/I/71E4TOjlmCL._AC_UF1000,1000_QL80_.jpg)
+
+ 
+
+ 
---
+++ superlists/functional_tests.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+import unittest
+
+
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_if_can_start_a_list_and_retrieve_it_later(self):
+        #Usuária Maria ouviu falar de app online para listar tarefas
+        #Maria decidiu ir até homepage
+        #Percebe que o título menciona "To-Do"
+        self.browser.get('http://localhost:8000')
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
