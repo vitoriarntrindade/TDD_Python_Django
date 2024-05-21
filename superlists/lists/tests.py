b8a89546447e3ba296f1e3183f83e33567f96638
vitoria <vitoriarntrindade@gmail.com>
2024-05-21 07:38:39 -0400

    Foi criada uma view básica e agora retorna um html simples ao usar o teste funcional

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,14 +1,25 @@
 from django.test import TestCase
 from django.urls import resolve
 from .views import home_page
+from django.http import HttpRequest
 
 
-class SmokeTest(TestCase):
-
-    def test_bad_math(self):
-        self.assertEqual(1 + 1, 3)
-
+# class SmokeTest(TestCase):
+#
+#     def test_bad_math(self):
+#         self.assertEqual(1 + 1, 3)
+#
 
 class HomePageTest(TestCase):
     def test_root_url_resolve_to_home_page_view(self):
         found = resolve('/')
+        self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startwith('<html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.endswith('</html>'))
+
