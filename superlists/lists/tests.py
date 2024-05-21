83b2874daaeea005e0155f29297fa861cc601aee
vitoria <vitoriarntrindade@gmail.com>
2024-05-21 17:36:07 -0400

    Model Item criada e makemigrations

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,6 +1,28 @@
 from django.test import TestCase
 from .views import home_page
 from django.http import HttpRequest
+from .models import Item
+
+
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = "first ever list item"
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = "item the second"
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        print(f"Total de itens salvos: {saved_items.count()}")
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, "first ever list item")
+        self.assertEqual(second_saved_item.text, "item the second")
 
 
 class HomePageTest(TestCase):
