09cac6a7c262582d02128e9de9343572c3f27bb1
vitoria <vitoriarntrindade@gmail.com>
2024-05-22 10:33:04 -0400

    Foi criado um novo cenário de teste simulando acesso de outro usuário, retornando um erro esperado

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -1,9 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-import unittest
+from selenium.common.exceptions import WebDriverException
 import time
 from django.test import LiveServerTestCase
+
+MAX_WAIT = 10
 
 
 class NewVisitorTest(LiveServerTestCase):
@@ -14,19 +16,37 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element(By.ID, 'id_list_table')
-        rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        #auxilia a testar se
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element(By.ID, 'id_list_table')
+                rows = table.find_elements(By.TAG_NAME, 'tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
     def test_if_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get(self.live_server_url)
-        self.assertIn('To-Do', self.browser.title)
-        header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
-        self.assertIn('To-Do', header_text)
         inputbox = self.browser.find_element(By.ID, 'id_new_item')
         inputbox.send_keys('buy a new notebook')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(3)
+        time.sleep(0.5)
 
-        self.check_for_row_in_list_table('buy a new notebook')
+        self.wait_for_row_in_list_table('1: buy a new notebook')
+
+        #o usuário recebe um url único
+
+        client_list_url = self.browser.current_url
+        self.assertRegex(client_list_url, '/lists/.+') #verifica se a string corresponde com a regex
+        self.browser.quit()
+
+        #simulando outro usuário acessando e sem poder ver os dados inseridos de outro usuário
+        self.browser = webdriver.Firefox()
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element(By.TAG_NAME, 'body').text
+        self.assertNotIn('1: buy a new notebook', page_text)
